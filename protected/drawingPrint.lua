43ff327bb42fed306e0d7eaa5253bfae20af47c7
K1xzy <109889881+K1xzy@users.noreply.github.com>
2023-04-14 18:50:51 -0400

    Update drawingPrint.lua

--- protected/drawingPrint.lua
+++ protected/drawingPrint.lua
@@ -2,40 +2,49 @@ local drawing = Drawing.new('Text')
 drawing.Size = 28
 drawing.Center = true
 drawing.Color = Color3.new(1, 1, 1)
+
 local lines = {}
+local maxLines = 3
 local m = game:GetService('Players').LocalPlayer:GetMouse()
+
 local update = function()
-  drawing.Text = table.concat(lines, '\n')
-  drawing.Position = Vector2.new(m.ViewSizeX / 2, m.ViewSizeY - (drawing.Size * #lines))
+drawing.Text = table.concat(lines, '\n')
+drawing.Position = Vector2.new(m.ViewSizeX / 2, m.ViewSizeY - (drawing.Size * #lines))
 end
-local d = function(...)
-  local str = ''
-  for idx, o in pairs { ... } do
-    if idx > 1 then
-      str = str .. ' '
-    end
-    str = str .. tostring(o)
-  end
-  table.insert(lines, str)
-  if #lines > 10 then
-    table.remove(lines, 1)
-  end
-  update()
-  task.spawn(function()
-    task.wait(8)
-    for idx, line in pairs(lines) do
-      if line == str then
-        table.remove(lines, idx)
-        update()
-        break
-      end
-    end
-  end)
+
+local printFunction = function(...)
+local str = ''
+for idx, o in pairs { ... } do
+if idx > 1 then
+str = str .. ' '
 end
+str = str .. tostring(o)
+end
+
+if #lines >= maxLines then
+table.remove(lines, 1)
+end
+
+table.insert(lines, str)
+update()
+
+task.spawn(function()
+task.wait(8)
+for idx, line in pairs(lines) do
+if line == str then
+table.remove(lines, idx)
+update()
+break
+end
+end
+end)
+end
+
 -- prevent gc + whatnot
 task.spawn(function()
-  while task.wait() do
-    drawing.Visible = true
-  end
+while task.wait() do
+drawing.Visible = true
+end
 end)
-return d
+
+return printFunction
